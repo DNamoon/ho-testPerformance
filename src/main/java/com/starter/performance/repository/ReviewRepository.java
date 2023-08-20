99188d3dd89a562a08d0a75572f7c53b92fa127f
Ho <leekoo3086@naver.com>
2023-08-21 01:33:23 +0900

    feat : 후기 작성 기능 - 예외 처리 내용 추가2 (OnlyOneReviewException) , 에러 메시지 상수로 변경 + 예매 한 건당 하나의 후기만 작성 예외 추가

--- src/main/java/com/starter/performance/repository/ReviewRepository.java
+++ src/main/java/com/starter/performance/repository/ReviewRepository.java
@@ -1,10 +1,12 @@
 package com.starter.performance.repository;
 
+import com.starter.performance.domain.Reservation;
 import com.starter.performance.domain.Review;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-
+    boolean existsByReservation(Reservation reservation);
 }
