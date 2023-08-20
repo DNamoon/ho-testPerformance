55d3e2c13501725274660fe7e9d77512b4ac49d3
Ho <leekoo3086@naver.com>
2023-08-20 17:46:42 +0900

    chore : 개인용 공연 프로젝트 생성

---
+++ src/main/java/com/example/hotestperformance/HoTestPerformanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.hotestperformance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoTestPerformanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoTestPerformanceApplication.class, args);
+    }
+
+}
